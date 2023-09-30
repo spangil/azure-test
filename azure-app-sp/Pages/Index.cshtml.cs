09d3989007d26dbe0d262a07403ed93ba1493506
Sunil Pangil <sunilpangil@outlook.com>
2023-09-30 16:58:04 -0400

    added config greeting

--- azure-app-sp/Pages/Index.cshtml.cs
+++ azure-app-sp/Pages/Index.cshtml.cs
@@ -6,15 +6,17 @@ namespace azure_app_sp.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
         {
             _logger = logger;
+            _config = config;
         }
 
         public void OnGet()
         {
-
+            ViewData["Greeting"] = _config["Greeting"];
         }
     }
 }
--- azure-app-sp/Pages/Privacy.cshtml.cs
+++ azure-app-sp/Pages/Privacy.cshtml.cs
@@ -14,6 +14,7 @@ namespace azure_app_sp.Pages
 
         public void OnGet()
         {
+            throw new Exception("Testing Insights");
         }
     }
 }
